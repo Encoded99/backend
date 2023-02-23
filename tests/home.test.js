e67b7a8e881ad75fd0887990380e402f7016a815
Nielvid <56310246+nielvid@users.noreply.github.com>
2023-02-23 18:42:54 +0100

    db config updated

---
+++ tests/home.test.js
@@ -0,0 +1,11 @@
+// const { a } = require('./index')
+
+test('should be 3', () => {
+  expect(1 + 2).toBe(3)
+})
+
+describe('should be 5', () => {
+  it('be equals to 5', () => {
+    expect(4 + 1).toBe(5)
+  })
+})
